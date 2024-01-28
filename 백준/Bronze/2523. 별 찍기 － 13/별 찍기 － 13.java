26a31fc4bf0356c1b891feaedcf982aad3954809
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-01-28 22:52:54 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 132 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 1; j < N - i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
