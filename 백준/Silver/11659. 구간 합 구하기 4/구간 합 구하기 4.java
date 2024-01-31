fd03ea21d6a6b061d71d4180a8f16f0d5f948741
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-01-31 17:13:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 596 ms, Memory: 57072 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,29 +2,32 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static int[] sums;
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        int[] sum = new int[N+1];
+        arr = new int[N + 1];
+        sums = new int[N + 1];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        sum[0] = 0;
         for (int i = 1; i <= N; i++) {
-            sum[i] = sum[i-1]+arr[i-1];
+            arr[i] = Integer.parseInt(st.nextToken());
+            //이전 누적합 + 자기 자신의 값
+            sums[i] = sums[i - 1] + arr[i];
         }
-        
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            sb.append(sum[end]-sum[start-1]).append("\n");
+
+            sb.append(sums[end] - sums[start - 1]).append("\n");
         }
         bw.write(sb.toString());
         bw.flush();
