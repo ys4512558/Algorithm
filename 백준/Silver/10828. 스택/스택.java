667e2b32ca4d8b1bbaae02997c0015c41cabc172
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-02-07 20:00:29 +0900

    [Silver IV] Title: 스택, Time: 140 ms, Memory: 17172 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,12 +1,10 @@
 import java.io.*;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
@@ -16,17 +14,68 @@ public class Main {
             if(cmd.equals("push")){
                 stack.push(Integer.valueOf(st.nextToken()));
             } else if(cmd.equals("pop")){
-                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                sb.append(stack.pop()).append("\n");
             } else if(cmd.equals("size")){
                 sb.append(stack.size()).append("\n");
             } else if(cmd.equals("empty")){
-                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                sb.append(stack.empty() ? 1 : 0).append("\n");
             } else{
-                sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                sb.append(stack.top()).append("\n");
             }
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+    }
+}
+
+class Stack<E>{
+    StackNode<E> top;
+    StackNode<Integer> minus = new StackNode<>(-1);
+    int size;
+    public Stack() {
+        this.top = new StackNode<>();
+        size = 0;
+    }
+
+    public void push(E e) {
+        StackNode<E> stackNode = new StackNode<>(e);
+        stackNode.link = top;
+        top = stackNode;
+        size++;
+    }
+
+    public E pop() {
+        if (empty()) return (E) minus.element;
+        StackNode<E> stackNode = top;
+        top = stackNode.link;
+        size--;
+        return stackNode.element;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean empty(){
+        return size == 0;
+    }
+
+    public E top(){
+        if (empty()) return (E) minus.element;
+        return top.element;
+    }
+}
+
+class StackNode<T>{
+    T element;
+    StackNode<T> link;
+
+    public StackNode() {
+    }
+
+    public StackNode(T element) {
+        this.element = element;
+        this.link = null;
     }
 }
