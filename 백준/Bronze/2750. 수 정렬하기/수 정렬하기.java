79efc60d75db7847fb0ad62ef86850619f4ac146
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-02-10 01:25:44 +0900

    [Bronze II] Title: 수 정렬하기, Time: 100 ms, Memory: 11996 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -2,27 +2,33 @@ import java.io.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                if(arr[i] > arr[j]) swap(arr, i, j);
-            }
-        }
+        selectionSort(arr);
+
         for (int i = 0; i < N; i++) {
             sb.append(arr[i]).append("\n");
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+    }
+
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                if(arr[i] > arr[j]) swap(arr, i, j);
+            }
+        }
     }
 
     private static void swap(int[] arr, int i, int j) {
