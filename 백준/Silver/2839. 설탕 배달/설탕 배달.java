d5ef321c9e7081d69c36353da407c6e8fe8e26f1
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-02-13 16:08:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 12124 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,40 +1,37 @@
 import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder();
-
+    static int[] dp;
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        sb.append(sugar(N, dp));
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new int[N + 1];
+
+        memo(N);
+        sb.append(dp[N]);
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
 
-    private static int sugar(int n, int[] dp) {
-        if(dp[n] != 0){
-            return dp[n];
+    private static int memo(int cur) {
+        if(dp[cur] != 0) return dp[cur];
+        if(cur == 3 || cur == 5) return dp[cur] = 1;
+
+        if(cur < 3 || cur == 4){
+            return dp[cur] = -1;
         }
-        if (n < 3 || n == 4) {
-            return -1;
-        }
-        if (n == 3 || n == 5) {
-            dp[n] = 1;
-            return dp[n];
-        }
-        int res5 = sugar(n - 5, dp);
-        int res3 = sugar(n - 3, dp);
-        if (res5 == -1 && res3 == -1) {
-            dp[n] = -1;
-        } else if (res5 == -1 || res3 == -1) {
-            dp[n] = Math.max(res5, res3) + 1;
-        } else{
-            dp[n] = Math.min(res5, res3) + 1;
-        }
-        return dp[n];
+
+        int res1 = memo(cur - 5);
+        int res2 = memo(cur - 3);
+
+        if(res1 != -1 && res2 != -1) return dp[cur] = Math.min(res1, res2) + 1;
+
+        return dp[cur] = res1 == -1 && res2 == -1 ? -1 :
+                ((res2 == -1) ? res1 : res2) + 1;
     }
 }
