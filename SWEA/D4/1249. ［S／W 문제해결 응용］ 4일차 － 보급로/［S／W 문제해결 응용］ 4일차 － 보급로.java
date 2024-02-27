2dda3f853dfecef11c3cbb30e2ab24351ead3dee
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-02-27 14:14:33 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 469 ms, Memory: 88,816 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,78 +1,71 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
         for (int i = 1; i <= T; i++) {
             sb.append("#" + i + " ");
-            sb.append(solve()).append("\n");
+            sb.append(solve1249()).append("\n");
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
 
-    private static int solve() throws IOException {
+    private static long solve1249() throws IOException {
         int N = Integer.parseInt(br.readLine());
-
-        int map[][] = new int[N][N];
-
+        int[][] map = new int[N][N];
+        long[][] res = new long[N][N];
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = (input.charAt(j) - '0');
+                map[i][j] = Integer.parseInt(String.valueOf(input.charAt(j)));
+                res[i][j] = -1;
             }
         }
 
-        return bfs(map, N);
+
+
+        return bfs1249(map, res);
     }
 
-    private static int bfs(int[][] map, int n) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        boolean[][] isv = new boolean[n][n];
-        pq.offer(new Pair(0, 0, 0));
-        isv[0][0] = true;
-        int weight = 0;
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-
-            if (p.x == n - 1 && p.y == n - 1) {
-                weight = p.weight;
-                break;
+    private static long bfs1249(int[][] map, long[][] res) {
+        class Point{
+            int x, y;
+            long score;
+            public Point(int x, int y, long score) {
+                this.x = x;
+                this.y = y;
+                this.score = score;
             }
+        }
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
 
+        int N = map.length;
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(0, 0, 0));
+
+        while (!queue.isEmpty()) {
+            Point cur = queue.poll();
             for (int i = 0; i < 4; i++) {
-                int x = p.x + dx[i];
-                int y = p.y + dy[i];
-
-                if (x < 0 || x >= n || y < 0 || y >= n || isv[x][y]) continue;
-                isv[x][y] = true;
-                pq.offer(new Pair(x, y, p.weight + map[x][y]));
+                int row = cur.y + dy[i];
+                int col = cur.x + dx[i];
+                if (row < 0 || row >= N || col < 0 || col >= N) {
+                    continue;
+                }
+                if (res[row][col] != -1 && res[row][col] <= cur.score + map[row][col]) {
+                    continue;
+                }
+                res[row][col] = cur.score + map[row][col];
+                queue.add(new Point(col, row, res[row][col]));
             }
         }
-        return weight;
-    }
-}
-
-class Pair implements Comparable<Pair>{
-    int x, y, weight;
-
-    public Pair(int x, int y, int weight) {
-        this.x = x;
-        this.y = y;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Pair o) {
-        return Integer.compare(this.weight, o.weight);
+        return res[N - 1][N - 1];
     }
 }
