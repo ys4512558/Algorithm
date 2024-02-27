db3cbc14d9c1e1abda55212c0dae158454ff419e
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-02-27 13:23:31 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,831 ms, Memory: 117,660 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,16 +1,12 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-
+    static int[] parents, rank;
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= T; i++) {
@@ -24,69 +20,81 @@ public class Solution {
 
     private static long solve() throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        ArrayList<ArrayList<Vertex>> adjList = new ArrayList<ArrayList<Vertex>>(N + 1);
-        for (int i = 0; i <= N; i++) {
-            adjList.add(new ArrayList<>());
+        Edge[] edges = new Edge[E];
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(from, to, weight);
         }
 
-        for (int i = 0; i < M; i++) {
-            StringTokenizer stk = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(stk.nextToken());
-            int to = Integer.parseInt(stk.nextToken());
-            int weight = Integer.parseInt(stk.nextToken());
-            adjList.get(from).add(new Vertex(to, weight));
-            adjList.get(to).add(new Vertex(from, weight));
-        }
+        Arrays.sort(edges);
 
-        return prim(adjList);
-    }
-
-    private static long prim(ArrayList<ArrayList<Vertex>> adjList) {
-        int[] minEdge = new int[N + 1];
-        boolean[] isv = new boolean[N + 1];
-
-        Arrays.fill(minEdge, Integer.MAX_VALUE);
-        PriorityQueue<Vertex> pq = new PriorityQueue<>();
-        minEdge[1] = 0;
-        pq.offer(new Vertex(1, minEdge[1]));
-        long result = 0;
-
-        while (!pq.isEmpty()) {
-            Vertex v = pq.poll();
-
-            if(isv[v.node]) continue;
-            result += v.weight;
-            isv[v.node] = true;
-
-            ArrayList<Vertex> list = adjList.get(v.node);
-            for (int i = 0; i < list.size(); i++) {
-                int no = list.get(i).node;
-                int weight = list.get(i).weight;
-                if (!isv[no] && weight < minEdge[no]) {
-                    minEdge[no] = weight;
-                    pq.offer(new Vertex(no, weight));
-                }
+        makeSet(V);
+        int count = V - 1;
+        long weight = 0;
+        for (int i = 0; i < E; i++) {
+            int from = edges[i].from;
+            int to = edges[i].to;
+            if (union(from, to)) {
+                count++;
+                weight += edges[i].weight;
             }
+            if(count == 0) break;
         }
 
-        return result;
+        return weight;
     }
+
+    private static void makeSet(int v){
+        parents = new int[v + 1];
+        rank = new int[v + 1];
+
+        for (int i = 1; i <= v; i++) {
+            parents[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    private static int find(int v) {
+        if(parents[v] == v) return v;
+        return parents[v] = find(parents[v]);
+    }
+
+    private static boolean union(int v1, int v2){
+        int rep1 = find(v1);
+        int rep2 = find(v2);
+        if (rep1 == rep2) return false;
+
+        if (rank[rep1] < rank[rep2]) {
+            parents[rep1] = rep2;
+            return true;
+        }
+        parents[rep2] = rep1;
+        rank[rep1] = rank[rep1] == rank[rep2] ? rank[rep1] + 1 : rank[rep1];
+        return true;
+    }
+
+
+
 }
 
-class Vertex implements Comparable<Vertex>{
-    int node;
-    int weight;
+class Edge implements Comparable<Edge>{
+    int from, to, weight;
 
-    public Vertex(int node, int weight) {
-        this.node = node;
+    public Edge(int from, int to, int weight) {
+        this.from = from;
+        this.to = to;
         this.weight = weight;
     }
 
     @Override
-    public int compareTo(Vertex o) {
+    public int compareTo(Edge o) {
         return Integer.compare(this.weight, o.weight);
     }
 }
