da910b3c526feef84e541bc0d94deea3480ca049
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-03-01 16:11:50 +0900

    [Silver I] Title: 회의실 배정, Time: 592 ms, Memory: 46564 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] times = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			times[i][0] = Integer.parseInt(st.nextToken());
+			times[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(times, (o1, o2)-> {
+			if(o1[0] == o2[0]) {
+				return Integer.compare(o1[1], o2[1]);
+			}
+			return Integer.compare(o1[0], o2[0]);
+		});
+		int cnt = 1;
+		int start = times[0][0];
+		int end = times[0][1];
+		for (int i = 1; i < times.length; i++) {
+			if(end > times[i][1]) { //이전에 선택한 회의가 지금 확인하는 회의보다 늦게 끝나는 경우
+				//이전 회의 대신 선택
+				start = times[i][0];
+				end = times[i][1]; 
+			} else if(end <= times[i][0]) { //현재 추가하려는 회의가 이전 회의 종료 시간보다 늦게 시작하면 추가
+				cnt++;
+				start = times[i][0];
+				end = times[i][1];
+			}
+		}
+		System.out.println(cnt);
+	}
+}
