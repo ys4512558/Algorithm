2b946436c88374332a07a1d2743df44421984447
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-03-11 20:33:37 +0900

    [Silver I] Title: 문자열 교환, Time: 240 ms, Memory: 18852 KB -BaekjoonHub

---
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        int size = input.length();
+        int countA = (int) input.chars().filter((value -> value == 'a')).count();
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < size; i++) {
+            int countB = 0;
+            for (int j = 0; j < countA; j++) {
+                if(input.charAt((i + j) % size) == 'b') countB++;
+            }
+            min = Math.min(min, countB);
+        }
+        sb.append(min);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
