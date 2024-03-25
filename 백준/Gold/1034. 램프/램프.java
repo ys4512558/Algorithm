16a29945101cafb25e180ddbbf0b603c1b2778f2
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-03-25 15:58:37 +0900

    [Gold IV] Title: 램프, Time: 80 ms, Memory: 11772 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.java
+++ 백준/Gold/1034. 램프/램프.java
@@ -1,8 +1,12 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, K, max = 0;
+    static int N, M, K, max;
+    static Map<String, Integer> map = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,54 +15,24 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][M];
-
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = line.charAt(j) - '0';
+            Integer value = map.putIfAbsent(line, 1);
+            if (value != null) {
+                map.put(line, value + 1);
             }
         }
         int K = Integer.parseInt(br.readLine());
-
-        Loop:
-        for (int i = 0; i < N; i++) {
+        for (String key : map.keySet()) {
             int cnt = 0;
-            boolean[] flags = new boolean[M];
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 0) {
-                    cnt++;
-                    flags[j] = true;
-                }
-                if (cnt > K) continue Loop;
+            for (int i = 0; i < key.length(); i++) {
+                if(key.charAt(i) == '0') cnt++;
             }
-            if ((cnt % 2) == (K % 2)) {
-                toggle(arr, flags);
-            }
+            if(cnt > K || (cnt % 2 != K % 2)) continue;
+            max = Math.max(max, map.get(key));
         }
         bw.write(String.valueOf(max));
         bw.flush();
         bw.close();
-    }
-
-    private static void toggle(int[][] arr, boolean[] flags) {
-        int[][] temp = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                temp[i][j] = flags[j] ? arr[i][j] ^ 1 : arr[i][j];
-            }
-        }
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            boolean flag = true;
-            for (int j = 0; j < M; j++) {
-                if (temp[i][j] == 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) cnt++;
-        }
-        max = Math.max(max, cnt);
     }
 }
