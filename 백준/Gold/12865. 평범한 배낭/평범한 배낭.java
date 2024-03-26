6932db91fe3fdd978006db0fe00a9d3a72715dae
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-03-26 22:03:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,25 +4,26 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[N + 1][K + 1];
+        int[] dp = new int[K + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             int W = Integer.parseInt(st.nextToken());
             int V = Integer.parseInt(st.nextToken());
-            for (int j = 1; j <= K; j++) {
-                if (W > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W] + V);
-                }
+            for (int j = K; j >= 0; j--) {
+                if (W > j) continue;
+                dp[j] = Math.max(dp[j], dp[j - W] + V);
             }
         }
-        System.out.println(dp[N][K]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(dp[K]);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
