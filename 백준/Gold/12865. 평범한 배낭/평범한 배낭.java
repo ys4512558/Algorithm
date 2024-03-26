e3ba0a6a5b288a0c0a5c125983b4ef393f57e271
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-03-26 12:27:56 +0900

    [Gold V] Title: 평범한 배낭, Time: 204 ms, Memory: 52560 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= K; j++) {
+                if (W > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W] + V);
+                }
+            }
+        }
+        bw.write(String.valueOf(dp[N][K]));
+        bw.flush();
+        bw.close();
+    }
+}
