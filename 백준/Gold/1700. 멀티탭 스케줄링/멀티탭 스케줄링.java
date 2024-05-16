0c4b85b6658e6f1df833fc00f9c8b3b1eb8e647b
WangSeungCheol <31546750+ys4512558@users.noreply.github.com>
2024-05-16 12:18:51 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 76 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -8,9 +8,11 @@ import java.util.StringTokenizer;
 
 
 public class Main {
+    //해당 번호의 가전제품이 사용중인지 확인하는 Map
     static Map<Integer, Boolean> holes;
     static Appliance[] appliances;
     static int[] use;
+    //현재 사용중인 가전제품 (전원이 연결된 상태인 가전제품)
     static PriorityQueue<Appliance> usedAppliances;
     static int N, K;
     public static void main(String[] args) throws IOException {
@@ -37,22 +39,28 @@ public class Main {
             appliances[idx].times.offer(i);
         }
 
+        //현재까지 꽂힌 가전제품 수
         int usedCount = 0;
         int cnt = 0;
         for (int i = 1; i <= K; i++) {
             int idx = use[i];
-
+    
+            //사용중인 콘센트 보다 콘센트가 더 많을때
             if (usedCount < N) {
+                //새로운 가전제품이면 그냥 추가
                 if (!holes.get(idx)) {
                     appliances[idx].count--;
                     appliances[idx].times.poll();
                     usedAppliances.offer(appliances[idx]);
                     holes.put(appliances[idx].num, true);
                     usedCount++;
-                } else {
+                } else { 
+                    //이미 꽂혀있는 가전제품이면 PQ갱신을 위해 원래 가전제품을 PQ에서 제거
                     usedAppliances.remove(appliances[idx]);
+                    //갱신 로직 수행 후
                     appliances[idx].count--;
                     appliances[idx].times.poll();
+                    //다시 추가
                     usedAppliances.offer(appliances[idx]);
                 }
             } else if (!holes.get(idx)) { //모두 사용하고 있고 꽂혀있지 않은 제품인 경우
@@ -62,7 +70,9 @@ public class Main {
                 usedAppliances.offer(appliances[idx]);
                 holes.put(appliances[idx].num, true);
                 cnt++;
-            } else { //꽂혀있는 제품인 경우 갱신해서 다시 넣어줘야함.
+            } else { 
+                //위와 동일(이렇게 하지 않으면 갱신되지 않은 상태)
+                //(정렬상태가 변경되기 전의 상태로 남아있다)
                 usedAppliances.remove(appliances[idx]);
                 appliances[idx].count--;
                 appliances[idx].times.poll();
@@ -86,13 +96,21 @@ class Appliance implements Comparable<Appliance> {
 
     @Override
     public int compareTo(Appliance o) {
+        //정렬 기준 (뭘 먼저 뽑을것인지)
+        //만약 둘 다 더 이상 사용되지 않는다면 아무나 상관없다.
         if (this.times.isEmpty() && o.times.isEmpty()) {
             return 0;
+        //둘 중 하나가 더 이상 사용되지 않으면 해당 우선
         } else if (this.times.isEmpty()) {
             return -1;
         } else if (o.times.isEmpty()) {
             return 1;
         } else {
+            //둘 다 이후 사용된다면 가장 가까운 사용 시간이 더 먼 것
+            //(1) -> {3, 5, 7}
+            //(2) -> {4, 6, 8}
+            //가장 가까운 사용 시간 (1) = `3`, (2) -> `4`
+            //(2)가 더 늦게 사용되므로 이를 우선순위 먼저
             return Integer.compare(o.times.peek(), this.times.peek());
         }
     }
