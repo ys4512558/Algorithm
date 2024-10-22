8f891646fe10edaf114f9d1f804f9378176ed4dd
WangSeungCheol <ys4512558@naver.com>
2024-10-22 16:15:54 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 96 ms, Memory: 12756 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
@@ -24,11 +24,14 @@ public class Main {
         Arrays.sort(pairs);
         int max = 0;
         for (int i = 0; i < K; i++) {
+            //x 좌표를 별똥별 중에 하나 선택
             int x = pairs[i].x;
             for (int j = 0; j < K; j++) {
+                //y 좌표를 별똥별 중에 하나 선택
                 int y = pairs[j].y;
                 int cnt = 0;
                 for (int k = 0; k < K; k++) {
+                    //선택한 별똥별이 x ~ x + L && y ~ y + L 사이에 있는지 확인 후 카운팅
                     if (pairs[k].x >= x && pairs[k].y >= y &&
                         pairs[k].x <= x + L && pairs[k].y <= y + L) {
                         cnt++;
