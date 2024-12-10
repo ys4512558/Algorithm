94af4c249cedbc67b9b6ac0c7078ef52e243114d
WangSeungCheol <ys4512558@naver.com>
2024-12-10 17:43:25 +0900

    [Gold IV] Title: 팰린드롬?, Time: 764 ms, Memory: 233232 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.kt
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.kt
@@ -8,18 +8,18 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     var N = readLine().toInt()
     var st = StringTokenizer(readLine())
     var array = Array(N, { st.nextToken().toInt()})
-    var dp = Array(N, { BooleanArray(N, { true }) })
+    var dp = Array(N, { BooleanArray(N, { false }) })
 
-    for (i in 0..N - 1) {
-        for (j in 0..N - 1) {
-            var s = i
-            var e = j
-            while (s < e) {
-                if (array[s++] != array[e--]) {
-                    dp[i][j] = false
-                    break
-                }
-            }
+    //길이를 기준으로 체크 (길이가 1이면 무조건 true)
+    for (i in 0 until N) { dp[i][i] = true}
+    //길이가 2일때 인접한 두 문자가 같으면 true
+    for (i in 0 until N - 1) { dp[i][i + 1] = (array[i] == array[i + 1]) }
+    //길이가 3이상일때
+    for (len in 3 .. N) {
+        for (s in 0 .. N - len) { //시작점
+            var e = s + len - 1 //끝점
+            //시작점과 끝점이 같으며 시작점 - 끝점 바로 전 (s = 1, e = 5 (길이 5)일때 2 ~ 4(길이 3) 구간은 이미 구해진 상태)
+            dp[s][e] = (array[s] == array[e] && dp[s + 1][e - 1])
         }
     }
 
