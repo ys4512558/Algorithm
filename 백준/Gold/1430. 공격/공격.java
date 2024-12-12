0639c4d90bfc8825c612e23f65deeaf3e83b023c
WangSeungCheol <ys4512558@naver.com>
2024-12-12 18:51:19 +0900

    [Gold IV] Title: 공격, Time: 200 ms, Memory: 18604 KB -BaekjoonHub

---
+++ 백준/Gold/1430. 공격/공격.java
@@ -0,0 +1,107 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static Set<Top> set;
+    static Map<Integer, Integer> map;
+    static Top[] tops;
+    static int N, R, D, X, Y;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //탑의 개수
+        N = Integer.parseInt(st.nextToken());
+        //사정거리
+        R = Integer.parseInt(st.nextToken());
+        //초기에너지
+        D = Integer.parseInt(st.nextToken());
+        //적의 X, Y좌표
+        X = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+
+        set = new HashSet<>();
+        map = new HashMap<>(); //어떤 depth에 속하는지 (적 - R(a) - R(b)) 이런식으로 계속 뻗어나가도록 할때 2차원 리스트에 각 범위별로관리
+
+        //최소 이동 -> 최대 효율
+        //적의 X, Y에서 시작해서 R 거리인 애들 A
+        //A에 속한 그룹 제외 A에서 R거리인 애들 B ... 반복
+        tops = new Top[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            tops[i] = new Top(x, y, D);
+        }
+        
+        bfs(new Top(X, Y, 0));
+        double res = 0;
+        for (int i : map.keySet()) {
+            int cnt = map.get(i);
+            res += (D * cnt) / Math.pow(2, i);
+        }
+        System.out.println(res);
+    }
+
+    private static void bfs(Top start) {
+        Queue<Top> queue = new ArrayDeque<>();
+        queue.offer(start);
+
+        int breadth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                Top top = queue.poll();
+
+                //이전탑을 기준으로 정렬
+                Arrays.sort(tops, (o1, o2) -> {
+                    double dist1 = getDist(o1.x, o1.y, top.x, top.y);
+                    double dist2 = getDist(o2.x, o2.y, top.x, top.y);
+                    return Double.compare(dist1, dist2);
+                });
+
+                for (int i = 0; i < N; i++) {
+                    //정렬했으므로 넘어가면 할필요없음
+                    if (getDist(tops[i].x, tops[i].y, top.x, top.y) > R) break;
+                    if (set.add(tops[i])){
+                        map.put(breadth, map.getOrDefault(breadth, 0) + 1);
+                        queue.offer(tops[i]);
+                    }
+                }
+            }
+            breadth++;
+        }
+    }
+
+    public static double getDist(int sx, int sy, int ex, int ey) {
+        return Math.sqrt(Math.pow(sx - ex, 2) + Math.pow(sy - ey, 2));
+    }
+}
+
+class Top {
+    int x, y;
+    double energy;
+
+    public Top(int x, int y, double energy) {
+        this.x = x;
+        this.y = y;
+        this.energy = energy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Top top = (Top) o;
+        return x == top.x && y == top.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
