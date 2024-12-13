2dd5824222cdb6af7c29fdff2c8fe4d6c289e889
WangSeungCheol <ys4512558@naver.com>
2024-12-13 17:09:33 +0900

    [Gold IV] Title: 타일 채우기, Time: 76 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.kt
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.kt
@@ -4,9 +4,9 @@ import java.io.InputStreamReader
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
 
     var N = readLine().toInt()
-    var dp = Array(31, { 0})
+    var dp = Array(N + 1, { 0})
 
-    dp[2] = 3
+    if(N > 1) dp[2] = 3
     for (i in 4..N step 2) {
         dp[i] = dp[i - 2] * dp[2]
         for (j in 2..i - 4 step 2) {
