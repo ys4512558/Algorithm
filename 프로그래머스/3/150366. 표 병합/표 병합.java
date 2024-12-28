490e7b65b22e652e13514cb631ec8a6e4ba12108
WangSeungCheol <ys4512558@naver.com>
2024-12-28 20:13:56 +0900

    [level 3] Title: 표 병합, Time: 108.72 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.java
+++ 프로그래머스/3/150366. 표 병합/표 병합.java
@@ -1,240 +1,270 @@
 import java.util.*;
 
 class Solution {
-    static final int N = 50;
-    static int[] parents;
-    //해당 flat한 셀이 가진 자식 (union-find의 반대)
-    //이를 통해 unMerge를 수행하기 위함
-    //기존 유니온 파인드는 부모를 기억하는데 unMerge를 위해서는 자식 정보가 필요함
-    //최상위 부모를 찾고 부모에서 자식을 역추적하며 parents를 자기자신으로 바꿔주면 unMerger가 된다고 생각
-    static List[] childs;
-    static int[] ranks;
-    static String[] values;
-    static Map<String, Set<Integer>> valueMap;
+    static final int MAX_SIZE = 50; // 표 최대 크기
+    
+    static String[][] graph; // 표
+    static int[] parent; // 부모 노드 표시(유니온 파인드)
+    
     public String[] solution(String[] commands) {
-        int idx = 0;
-        valueMap = new HashMap<>();
-        List<String> answerList = new ArrayList<>();
-        parents = new int[N * N];
-        childs = new ArrayList[N * N];
-        ranks = new int[N * N];
-        values = new String[N * N];
+        graph = new String[51][51];
+        // 인덱스 : (r, c) -> r*100 + c
+        // 5050 까지 인덱싱 해야돼서(0101: (1, 1), 5050 : (50, 50))
+        parent = new int[5051];
         
-        //유니온 파인드 초기화
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                int value = flat(i, j);
-                childs[value] = new ArrayList<Integer>();
-                parents[value] = value;
-                ranks[value] = 1;
-                values[value] = "";
+        // parent 초기화
+        init();
+        
+        List<String> answers = new ArrayList<>();
+        
+        for(String command : commands) {
+            String[] strings = command.split(" ");
+            String majorCommand = strings[0]; // UPDATE, MERGE, UNMERGE, PRINT
+            
+            int r, c, cellParentIdx, cellIndex;
+            String value;
+            
+            switch(majorCommand) {
+                case "UPDATE":
+                    // UPDATE value1 value2 케이스
+                    if(strings.length == 3) {
+                        String value1 = strings[1];
+                        String value2 = strings[2];
+
+                        updateValue(value1, value2);
+                    }
+                    // UPDATE r c value 케이스
+                    else {
+                        r = Integer.parseInt(strings[1]);
+                        c = Integer.parseInt(strings[2]);
+                        value = strings[3];
+
+                        cellParentIdx = find(position2Index(r, c));
+                        int[] parentPosition = index2Position(cellParentIdx);
+                        
+                        // 표 바꾸기
+                        graph[parentPosition[0]][parentPosition[1]] = value;
+                    }
+                    break;
+                case "MERGE":
+                    int r1 = Integer.parseInt(strings[1]);
+                    int c1 = Integer.parseInt(strings[2]);
+                    int r2 = Integer.parseInt(strings[3]);
+                    int c2 = Integer.parseInt(strings[4]);
+                    
+                    merge(r1, c1, r2, c2);
+                    break;
+                case "UNMERGE":
+                    r = Integer.parseInt(strings[1]);
+                    c = Integer.parseInt(strings[2]);
+
+                    // 해제
+                    disunion(r, c);
+                    break;
+                case "PRINT":
+                    r = Integer.parseInt(strings[1]);
+                    c = Integer.parseInt(strings[2]);
+                    
+                    String answer = findAnswer(r, c);
+                    
+                    // answers 리스트에 삽입
+                    answers.add(answer);
+                    break;
             }
+            print(command);
+            // printReal(command);
         }
         
-        for(int i = 0; i < commands.length; i++) {
-            StringTokenizer st = new StringTokenizer(commands[i]);
-            String command = st.nextToken();
-            
-            if(command.equals("UPDATE")) {
-                //남은 토큰 수로 어떤 업데이트인지 체크
-                int cnt = st.countTokens();
-                if(cnt == 3) {
-                    int r = Integer.parseInt(st.nextToken()) - 1;
-                    int c = Integer.parseInt(st.nextToken()) - 1;
-                    String value = st.nextToken();
-                    update(r, c, value);
-                } else {
-                    String oldValue = st.nextToken();
-                    String newValue = st.nextToken();
-                    update(oldValue, newValue);
+        // 정답 배열 만들기
+        String[] answerArray = new String[answers.size()];
+        for(int i=0; i<answers.size(); i++) {
+            answerArray[i] = answers.get(i);
+        }
+        return answerArray;
+    }
+    
+    /**
+    * 2번 케이스. UPDATE value1 value2 
+    * graph에서 value1을 value2로 바꾸기
+    **/
+    static void updateValue(String value1, String value2) {
+        for(int i=1; i<=MAX_SIZE; i++) {
+            for(int j=1; j<=MAX_SIZE; j++) {
+                if(graph[i][j] == null) continue;
+                if(graph[i][j].equals(value1)) {
+                    graph[i][j] = value2;
                 }
-            } else if(command.equals("MERGE")) {
-                int r1 = Integer.parseInt(st.nextToken()) - 1;
-                int c1 = Integer.parseInt(st.nextToken()) - 1;
-                int r2 = Integer.parseInt(st.nextToken()) - 1;
-                int c2 = Integer.parseInt(st.nextToken()) - 1;
-                merge(r1, c1, r2, c2);
-            } else if(command.equals("UNMERGE")) {
-                int r = Integer.parseInt(st.nextToken()) - 1;
-                int c = Integer.parseInt(st.nextToken()) - 1;
-                unMerge(r, c);
-            } else if(command.equals("PRINT")) {
-                int r = Integer.parseInt(st.nextToken()) - 1;
-                int c = Integer.parseInt(st.nextToken()) - 1;
-                String value = print(r, c);
-                answerList.add(value);
-            }  
+            }
         }
-        String[] answer = new String[answerList.size()];
-        for(int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
-        return answer;
     }
     
-    //셀 값 업데이트
-    //해당 셀 값을 value로 업데이트
-    public void update(int r, int c, String value) {
-        int rc = flat(r, c);
-        int p = find(rc);
-        values[p] = value;
-        Set<Integer> set = valueMap.getOrDefault(value, new HashSet<>());
-        set.add(p);
-        valueMap.put(value, set);
+    /**
+    * parent 배열 초기화
+    **/
+    static void init() {
+        // 0101 이 가장 작은 값이니((1,1)일 때의 값이므로) 이 값부터 초기화
+        for(int i=101; i<parent.length; i++) {
+            if(i%100 > 50) continue; // 51부터는 표 크기 벗어나니까 초기화 안해도 됨
+            parent[i] = i;
+        }
     }
     
-    //해당 값을 가진 모든 셀을
-    //newValue로 업데이트
-    //value : 위치 형태의 맵을 통해 List로 관리? Map<String, List<Integer>
-    //이렇게 하며 값에 대해 리스트를 순회하며 values의 값과 비교하여 여전히 해당 값이라면 업데이트?
-    //근데 그냥 업데이트가 많아서 시간이 터지려나??
-    public void update(String oldValue, String newValue) {
-        Set<Integer> oldSet = valueMap.getOrDefault(oldValue, new HashSet<>());
-        Set<Integer> newSet = valueMap.getOrDefault(newValue, new HashSet<>());
+    /**
+    * value 찾기
+    **/
+    static String findValue(int v) {
+        // 1. 루트 인덱스 찾기
+        int rootIndex = find(v);
+        // 2. 루트에 해당하는 표의 좌표 찾기
+        int[] rootPosition = index2Position(rootIndex);
+        // 3. 해당 좌표의 값 반환
+        return graph[rootPosition[0]][rootPosition[1]];
+    }
     
-        // for(int i = 0; i < N; i++) {
-        //     for(int j = 0; j < N; j++) {
-        //         int rc = flat(i, j);
-        //         if(values[rc].equals(oldValue)) {
-        //             values[rc] = newValue;
-        //         }
-        //     }
-        // }
-        for(int rc : oldSet) {
-            int p = find(rc);
-            if(values[p].equals(oldValue)) {
-                newSet.add(p);
-                values[p] = newValue;
+    /**
+    * find 함수 : v 인덱스에 대해 루트 노드 찾기
+    **/
+    static int find(int v) {
+        if(parent[v] == v) {
+            return v;
+        } else {
+            return parent[v] = find(parent[v]);
+        }
+    }
+    
+    /**
+    * 유니온
+    **/
+    static boolean union(int indexToMerge, int indexToBeMerged) {
+        int aRoot = find(indexToMerge); // a의 루트 노드
+        int bRoot = find(indexToBeMerged); // b의 루트 노드
+        
+        if(aRoot == bRoot) { // 루트 노드가 같으면 union 할 거 없음
+            return false;
+        }
+        
+        int[] bRootPosition = index2Position(bRoot);
+        graph[bRootPosition[0]][bRootPosition[1]] = null;
+        
+        parent[bRoot] = aRoot; // a에 병합하기
+        return true;
+    }
+    
+    /**
+    * A 와 B 병합
+    **/
+    static void merge(int r1, int c1, int r2, int c2) {
+        // r1, c1 에 해당하는 값
+        String value1 = findValue(position2Index(r1, c1));
+        // r2, c2 에 해당하는 값
+        String value2 = findValue(position2Index(r2, c2));
+        
+        // 병합하는 아이의 인덱스, 병합되는 아이의 인덱스
+        int indexToMerge, indexToBeMerged;
+        // value1이 없고, value2가 있을 경우 : cell2가 병합하는 친구, cell1이 병합되는 친구
+        if(value1 == null && value2 != null) {
+            indexToMerge = position2Index(r2, c2);
+            indexToBeMerged = position2Index(r1, c1);
+        } else { // 그 외에는 cell1이 병합하는 친구, cell2이 병합되는 친구
+            indexToMerge = position2Index(r1, c1);
+            indexToBeMerged = position2Index(r2, c2);
+        }
+        // 유니온
+        union(indexToMerge, indexToBeMerged);
+    }
+    
+    /**
+    * index에 대해 해제
+    * index의 루트 노드를 가리키고 있는 애들을 본인을 바라보게 하기
+    **/
+    static void disunion(int r, int c) {
+        int index = position2Index(r, c);
+        int root = find(index); // 루트 노드 인덱스
+        
+        int[] rootPosition = index2Position(root); // 루트 노드의 좌표
+        String value = graph[rootPosition[0]][rootPosition[1]]; // 루트 노드의 표 값
+        graph[rootPosition[0]][rootPosition[1]] = null; // 루트 노드가 가리키는 표 값 null 로 바꾸기
+        
+        for(int i=101; i<parent.length; i++) {
+            // 51부터는 표 크기 벗어나니까 대상이 아님(51일테니까 거기에 50을 더해서 다음 단계 검색)
+            // 예시) 1051(10,51) -> 1101(11, 1)
+            if(i%100 > 50) {
+                i += 50;
+            };
+            // 경로 압축 진행
+            find(i);
+        }
+        
+        for(int i=101; i<parent.length; i++) {
+            // 51부터는 표 크기 벗어나니까 대상이 아님(51일테니까 거기에 50을 더해서 다음 단계 검색)
+            // 예시) 1051(10,51) -> 1101(11, 1)
+            if(i%100 > 50) {
+                i += 50;
+            };            
+            // root 노드를 가리키고 있는 애들을 바꾸기
+            if(find(i) == root) {
+                parent[i] = i;
             }
         }
         
-        valueMap.put(oldValue, new HashSet<>());
-        valueMap.put(newValue, newSet);
+        // 병합 해제 할 때, 이미 값이 있으면 r, c에 해당 값 넣기
+        if(value != null)
+            graph[r][c] = value;
     }
     
-    //r1, c1, r2, c2를 병합
-    //같은 셀인 경우 : 무시
-    //인접하지 않은 경우 : 해당 두 셀만 병합
-    //두 셀 중 하나만 값을 가진 경우 : 해당 값을 세팅
-    //두 셀 다 값을 가진 경우 : r1, c1의 값으로 세팅
-    //의식의 흐름 : 그럼 이건 유니온 파인드로 부모만 알면 되겟다?
-    //r1, c1, r2, c2중 어디를 접근해도 병합된 셀로 접근하니까    
-    public void merge(int r1, int c1, int r2, int c2) {
-        // System.out.println("Mergered");
+    /**
+    * parent의 index에 해당하는 셀의 값 반환
+    * 만약 null이면 EMPTY 넣기
+    **/
+    static String findAnswer(int r, int c) {
+        int index = position2Index(r, c);
+        int root = find(index);
+        int[] rootPosition = index2Position(root); // 루트 노드의 좌표
+        String answer = graph[rootPosition[0]][rootPosition[1]]; // 루트 노드의 표 값
         
-        int p1 = find(flat(r1, c1));
-        int p2 = find(flat(r2, c2));
-        // System.out.println("p1 : " + p1);
-        // System.out.println("p2 : " + p2);
-        
-        //이미 같은 경우는 스킵
-        if(p1 == p2) return;
-        String value = "";
-        //r1, c1값이 있으면 그걸로 세팅
-        if(!values[p1].equals("")) {
-            value = values[p1];
-            //r2, c2값은 없애주기
-            values[p2] = "";
-        } else { //r2, c2에만 값이 있으면 그걸로 세팅
-            //r1, c1값은 없애주기
-            value = values[p2];
-            values[p1] = "";
-        }
-        //union 수행
-        if(ranks[p1] < ranks[p2]) {
-            parents[p1] = p2;
-            values[p2] = value;
-            //자식으로 세팅
-            childs[p2].add(p1);
-            return;
-        }
-        ranks[p1] = ranks[p1] == ranks[p2] ? ranks[p1] + 1 : ranks[p1];
-        values[p1] = value;
-        parents[p2] = p1;
-        //마찬가지로 자식으로 세팅
-        childs[p1].add(p2);
-        // printValue();
+        return answer == null ? "EMPTY" : answer;
     }
     
-    //병합 해제
-    //해제 전 셀이 값을 가지고 있었다면 해당 값을 세팅
-    //유니온 파인드 해제를 위해 추가적으로 저장해야한다.
-    //유니온 파인드는 부모를 기억하는 것
-    //그렇다면 반대의 경우도 저장해주어 역추적 가능하게 하면 unMerge가 가능하지 않을까?
-    public void unMerge(int r, int c) {
-        // System.out.println("unMergered");
-        
-        int rc = flat(r, c);
-        //최상위 부모 찾기
-        int p = find(rc);
-        //기존에 병합된 값
-        String value = values[p];
-        
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(p);
-        parents[p] = p;
-        ranks[p] = 1;
-        values[p] = "";
-        
-        //BFS로 자식을 모두 자기자신을 부모로 초기화
-        while(!queue.isEmpty()) {
-            int cell = queue.poll();
-            
-            List<Integer> childList = childs[cell];
-            for(int i = 0; i < childList.size(); i++) {
-                int child = childList.get(i);
-                queue.offer(child);
-                //자기자신을 부모로
-                parents[child] = child;
-                //랭크도 1
-                ranks[child] = 1;
-                //값도 빈값으로 초기화
-                values[child] = "";
-            }
-            childs[cell] = new ArrayList<Integer>();
-        }
-        //값 세팅
-        values[rc] = value;
+    /**
+    * (r, c)로 parent 배열의 인덱스 만들기
+    * (r, c) -> r * 100 + c 로 하면 r,c 에 대해 안 겹치고 인덱스 만들기 가능
+    **/
+    static int position2Index(int r, int c) {
+        return r * 100 + c;
     }
     
-    //해당 셀의 값을 출력
-    //비어 있다면 "EMPTY" 출력
-    public String print(int r, int c) {
-        int rc = flat(r,c);
-        int p = find(rc);
-        String value = values[p];
-        // printValue();
-        return value.equals("") ? "EMPTY" : value;   
+    /**
+    * parent 배열 인덱스를 (r, c) 로 바꾸기
+    **/
+    static int[] index2Position(int index) {
+        int[] res = new int[2];
+        res[0] = index / 100;
+        res[1] = index % 100;
+        return res;
     }
     
-    //부모를 찾는 find 메서드 (유니온 파인드)
-    public int find(int v) {
-        if(parents[v] == v) return v;
-        return parents[v] = find(parents[v]);
-    }
-    
-    //r, c를 flat하게 사용하기 위한 메서드
-    //의식의 흐름 : 2차원 배열에서 유니온 파인드를 하기 위해 flat이 더 효율적이라고 생각함
-    public int flat(int r, int c) {
-        return r * N + c;
-    }
-    
-    public void printValue(){
-        System.out.println("===============================");
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
-                int ij = flat(i, j);
-                System.out.print((values[ij].equals("") ? "[ ]" : values[ij] )+ " ");
+    static void print(String command) {
+        System.out.println(command + ": ----------출력---------");
+        for(int i=1; i<=4; i++) {
+            for(int j=1; j<=4; j++) {
+                System.out.print(graph[i][j] + " ");
             }
             System.out.println();
         }
-         System.out.println("------------------------------------");
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
-                int ij = flat(i, j);
-                System.out.print((parents[ij]) + " ");
+        System.out.println("----------------------");
+    }
+    
+    static void printReal(String command) {
+        System.out.println(command + ": ----------출력---------");
+        for(int i=1; i<=4; i++) {
+            for(int j=1; j<=4; j++) {
+                int index = position2Index(i, j);
+                int root = find(index);
+                int[] rootPosition = index2Position(root);
+                System.out.print(graph[rootPosition[0]][rootPosition[1]] + " ");
             }
             System.out.println();
         }
+        System.out.println("----------------------");
     }
 }
