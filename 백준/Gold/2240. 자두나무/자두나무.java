a3b6ba3c78e72028c56c2deddf6d556ac1123c35
WangSeungCheol <ys4512558@naver.com>
2025-02-21 16:11:23 +0900

    [Gold V] Title: 자두나무, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,41 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int T = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
-
-        //시간, 움직인 횟수, 1번위치, 2번 위치
-        int[][][] dp = new int[T + 1][W + 1][2];
-
-        int result = 0;
-        for (int i = 1; i <= T; i++) {
-            //위치 1, 2 -> 0, 1로 변경
-            int p = Integer.parseInt(br.readLine()) - 1;
-
-            //현재 시간 vs W최대 범위 중 더 작은 만큼 확인
-            for (int j = 0; j <= Math.min(i, W); j++) {
-                //i시간에 W움직였을 때 1번 위치인 경우
-                int cur = j % 2; //0이면 1위치, 1이면 2위치
-                int count = p == cur ? 1 : 0;
-                if (j == 0) {
-                    dp[i][j][0] = dp[i - 1][j][0] + count;
-                    result = Math.max(result, dp[i][j][0]);
+        
+        int[][] dp = new int[W + 1][2];
+        int max = 0;
+        for(int i = 1; i <= T; i++) {
+            int N = Integer.parseInt(br.readLine()) - 1;
+            for(int j = 0; j <= Math.min(W, i); j++) {
+                if(j == 0) {
+                    dp[0][0] += N == 0 ? 1 : 0;   
+                    max = Math.max(max, dp[0][0]);
                     continue;
                 }
-                //이전에 반대 편에 있었던 경우 (이번에 움직이는 경우)
-                //vs 이전에 움직이고 가만히 있던 경우
-                dp[i][j][cur] = Math.max(dp[i - 1][j - 1][(cur + 1) % 2], dp[i - 1][j][cur]) + count;
-                result = Math.max(result, dp[i][j][cur]);
-//                System.out.println("i = " + i + " j =" + j + " [0] : " + dp[i][j][0] + " [1] : " + dp[i][j][1]);
+                dp[j][N] = Math.max(dp[j][N], dp[j - 1][1 - N]) + 1;
+                // dp[j][1 - N] = Math.max(dp[j][1 - N], dp[j - 1][N]);
+                max = Math.max(max, dp[j][N]);
             }
         }
-        System.out.println(result);
+        System.out.println(max);
     }
 }
