0354864af8e17cb252e8d6a1d45a17bc517bfa38
WangSeungCheol <ys4512558@naver.com>
2025-04-06 20:35:30 +0900

    [Bronze IV] Title: 구멍, Time: 68 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/33571. 구멍/구멍.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'A' || c == 'b' || c == 'B' || c == 'd' || c == 'e'
+                    || c == 'D' || c == 'g' || c == 'o' || c == 'O' || c == 'p'
+                    || c == 'P' || c == 'q' || c == 'Q' || c == 'R' || c == '@') {
+                cnt++;
+                if(c == 'B') cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
