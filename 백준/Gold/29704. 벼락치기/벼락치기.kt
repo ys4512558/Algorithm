c078d3986738ea523bfaae24b5b3a6295070aa3b
WangSeungCheol <ys4512558@naver.com>
2025-06-21 20:41:05 +0900

    [Gold V] Title: 벼락치기, Time: 104 ms, Memory: 13788 KB -BaekjoonHub

---
+++ 백준/Gold/29704. 벼락치기/벼락치기.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var st = StringTokenizer(readLine())
+
+    val N = st.nextToken().toInt()
+    val T = st.nextToken().toInt()
+    val dp = IntArray(T + 1) {0}
+
+    var sum = 0
+    for (i in 0..<N) {
+        st = StringTokenizer(readLine())
+        val d = st.nextToken().toInt()
+        val m = st.nextToken().toInt()
+        sum += m
+        for (j in T downTo 0) {
+            if(j < d) break
+            dp[j] = max(dp[j], dp[j - d] + m)
+        }
+    }
+    println(sum - dp[T])
+}
